ca7f60c8b30739a9691769a01a81c7807aea9792
Shu Ib <shuib@pm.me>
2023-09-15 16:55:44 +0200

    script2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script>
+	import { onMount } from 'svelte'
 	import '../app.postcss'
+
+	// onMount(() => {
+	// 	window.crmPluginConfig = {
+	// 		 onlinechat: { 
+	// 		   locale: {
+	// 			powered_by: 'AIgentX Bot',
+	// 		   }
+	// 		 }, 
+	// 		};
+			
+			
+	// 		(function(a,m,o,c,r,xx){a[xx]={id:"745608",hash:"8b5ad26a64c25392f7c4a80d80342efd7ac66b7cffe4eb8d8f5d72917e1ccf78",locale:"en",inline:false,setMeta:function(p){this.params=(this.params||[]).concat([p])}};a[o]=a[o]||function(){(a[o].q=a[o].q||[]).push(arguments)};var d=a.document,s=d.createElement('script');s.async=true;s.id=xx+'_script';s.src='https://gso.kommo.com/js/button.js?1694787632';d.head&&d.head.appendChild(s)}(window,0,'crmPlugin',0,0,'crm_plugin'));
+	// })
 </script>
 
 <slot />
